db867d503f0fd8bc9a59a13905e363e8e52015a5
podroomdevelopers <podroomdevelopers@gmail.com>
2023-05-26 19:48:42 -0400

    first set of iac files

---
+++ test_cli.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest.mock import patch
+from io import StringIO
+import cli
+
+class TestCLI(unittest.TestCase):
+    def test_list_s3_files_command(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            cli.list_s3_files_command(cli.argparse.Namespace(bucket_name='my-bucket'))
+            output = fake_out.getvalue().strip()
+            self.assertEqual(output, 'No files found in the S3 bucket.')
+
+    def test_list_ecs_task_definition_versions_command(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            cli.list_ecs_task_definition_versions_command(cli.argparse.Namespace(service_name='my-service'))
+            output = fake_out.getvalue().strip()
+            self
---
+++ vpc.tf
@@ -0,0 +1,69 @@
+resource "aws_vpc" "sourcefuse_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "sourcefuse_vpc"
+  }
+}
+
+resource "aws_subnet" "sourcefuse_public_subnet_1" {
+  vpc_id     = aws_vpc.sourcefuse_vpc.id
+  cidr_block = "10.0.16.0/24"
+  availability_zone = "us-east-1e"
+  tags = {
+    Name = "sourcefuse_public_subnet_1"
+  }
+}
+
+resource "aws_subnet" "sourcefuse_public_subnet_2" {
+  vpc_id     = aws_vpc.sourcefuse_vpc.id
+  cidr_block = "10.0.12.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "sourcefuse_public_subnet_2"
+  }
+}
+
+resource "aws_subnet" "sourcefuse_private_subnet" {
+  vpc_id     = aws_vpc.sourcefuse_vpc.id
+  cidr_block = "10.0.3.0/24"
+  tags = {
+    Name = "sourcefuse_private_subnet"
+  }
+}
+
+resource "aws_subnet" "sourcefuse_private_subnet2" {
+  vpc_id     = aws_vpc.sourcefuse_vpc.id
+  cidr_block = "10.0.4.0/24"
+  tags = {
+    Name = "sourcefuse_private_subnet2"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.sourcefuse_vpc.id
+
+  tags = {
+    Name = "sourcefuse_igw"
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.sourcefuse_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "sourcefuse_public_route_table"
+  }
+}
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id      = aws_subnet.sourcefuse_public_subnet_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_subnet_2" {
+  subnet_id      = aws_subnet.sourcefuse_public_subnet_2.id
+  route_table_id = aws_route_table.public.id
+}
